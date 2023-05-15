9c04f216ce9a92d07888b5e4ee78c7b8729f739a
Nadezhda.S <nadviks@mail.ru>
2023-05-15 16:59:04 +0300

    DZ10

---
+++ DZ10/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.Clear();
+Console.Write("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("Вы ввели не трехзначное число!");
+}
+else
+{
+    Console.WriteLine(secondNumber(number));
+}
+
+int secondNumber(int n)
+{ int result = ((n / 10) % 10);
+return result;
+}
