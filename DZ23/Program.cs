4f92fbc3e35d58988bb5bcb81fc7a77da59519dc
Nadezhda.S <nadviks@mail.ru>
2023-05-18 17:18:00 +0300

    DZ23

---
+++ DZ23/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.Write("Введите положительное целое число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+Cube(numberN);
+
+void Cube(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count,3} {Math.Pow(count, 3)}");
+        count++;
+    }
+}
+
